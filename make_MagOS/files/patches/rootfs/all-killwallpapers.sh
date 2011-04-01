3742b2aa8b224112319a4f9f3da61a5f1da61eb7
magos-linux <m3for@mail.ru>
2011-04-01 19:54:17 +0400

    ntlmaps added, rootfs patches modified (kslash, preinit, etc)

--- make_MagOS/files/patches/rootfs/all-killwallpapers.sh
+++ make_MagOS/files/patches/rootfs/all-killwallpapers.sh
@@ -8,3 +8,5 @@ for a in usr/share/mdk/backgrounds/Mandriva-*.jpg ;do
     [ -e "$a" ] || continue
     ln -sf /usr/share/wallpapers/default_blue.jpg "$a"
 done
+
+find usr/share/apps/ksplash/Themes/Default -name background.png | grep -v 1600x1200 | xargs rm -f 
