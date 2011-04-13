1d34f0c37af83f227e24aa481e1292ff94b83aeb
magos-linux <m3for@mail.ru>
2011-04-13 21:55:21 +0400

    release 20110413

--- make_MagOS/files/patches/rootfs/all-themes.sh
+++ make_MagOS/files/patches/rootfs/all-themes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's|<stringvalue>/usr/share/gnome-panel/pixmaps/mandriva-panel.png</stringvalue>|<stringvalue></stringvalue>|' etc/gconf/gconf.xml.defaults/%gconf-tree
-sed -i 's|<stringvalue>Ia Ora Steel</stringvalue>|<stringvalue>Default</stringvalue>|' etc/gconf/gconf.xml.defaults/%gconf-tree
+sed -i 's|<stringvalue>/usr/share/gnome-panel/pixmaps/mandriva-panel.png</stringvalue>|<stringvalue></stringvalue>|' etc/gconf/gconf.xml.defaults/%gconf-tree.xml
+sed -i 's|<stringvalue>Ia Ora Steel</stringvalue>|<stringvalue>Default</stringvalue>|' etc/gconf/gconf.xml.defaults/%gconf-tree.xml
 sed -i 's|<default>Ia Ora Steel</default>|<default>Default</default>|' etc/gconf/schemas/desktop_gnome_interface.schemas
 sed -i 's|<default>Ia Ora Steel</default>|<default>Default</default>|' etc/gconf/schemas/metacity.schemas
 sed -i 's|<string>/usr/share/gnome-panel/pixmaps/mandriva-panel.png</string>|<string></string>|' etc/gconf/schemas/panel-default-setup.entries
@@ -11,10 +11,21 @@ sed -i 's|<name>Clearlooks</name>|<name>Default</name>|' etc/xdg/openbox/rc.xml
 DIRGTK=gtk-2.0
 DIRMETACITY=metacity-1
 DIROPENBOX=openbox-3
+rm -fr "usr/share/themes/Default/$DIRGTK" "usr/share/themes/Default/$DIRMETACITY" "usr/share/themes/Default/$DIROPENBOX"
 ln -sf "../MagOS/$DIRGTK"  "usr/share/themes/Default/$DIRGTK"
 ln -sf "../MagOS/$DIRMETACITY" "usr/share/themes/Default/$DIRMETACITY"
 ln -sf "../MagOS/$DIROPENBOX" "usr/share/themes/Default/$DIROPENBOX"
-rm -fr usr/share/emerald/theme
-ln -sf "themes/MagOS" usr/share/emerald/theme
+rm -f usr/share/emerald/theme/*
+cp -pf usr/share/emerald/themes/MagOS/* usr/share/emerald/theme
+
+for a in usr/share/apps/desktoptheme/* ;do
+  if [ -f "$a/metadata.desktop" ] ;then
+    if ! grep -q "defaultWallpaperTheme" "$a/metadata.desktop" ;then
+       echo -e  "\n[Wallpaper]\ndefaultWallpaperTheme=/usr/share/mdk/backgrounds/default.jpg" >> "$a/metadata.desktop"
+    else
+       sed -i 's|defaultWallpaperTheme=.*|defaultWallpaperTheme=/usr/share/mdk/backgrounds/default.jpg|' "$a/metadata.desktop"
+    fi
+  fi
+done
 
 exit 0
