733f44b66c518fe98633d5fcde9ff7d124fa4393
Anton Goroshkin <antihaos@gmail.com>
2011-11-20 21:40:02 +0700

    fix drakconf.real & MagOS-drakconf.sh adds

---
+++ make_MagOS/files/patches/rootfs/MagOS-drakconf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PFP=usr/share/locale/ru/LC_MESSAGES/drakconf
+msgunfmt $PFP.mo -o drakconf.po
+cat >>drakconf.po <<EOF
+
+msgid "Modules Management"
+msgstr "Управление модулями"
+
+msgid "Install Software & Create LZM/XZM module"
+msgstr "Установить приложения & Создать модуль LZM/XZM"
+
+msgid "Convert LZM/XZM modules"
+msgstr "Конвертация модулей LZM/XZM"
+
+msgid "Modules Manager"
+msgstr "Менеджер модулей"
+
+msgid "Configs Management"
+msgstr "Управление конфигурациями"
+
+msgid "Configs Manager"
+msgstr "Менеджер конфигурационными файлами"
+
+EOF
+
+msgfmt drakconf.po -o $PFP.mo 
+
+exit 0
