84f2b5501d33635ab1dbd5e7a9b3d5ba2ca2ce6b
root <root@MagOS_VBox.(none)>
2012-09-20 23:08:24 +0800

    add-modules contents modify

---
+++ make_modules/add-modules/make_mod_repository.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Описание: Собирает модули в репозиторий для MagOS
+# Дата: 18.09.2012
+# Автор: Горошкин Антон
+
+#---------------
+function make_mod_pack ()
+{
+    mod_name=$1
+    shift
+    mod_br=/mnt/live/memory/tmp/_fly_mods
+    root_br=/mnt/live/memory/tmp/_fly_rootfs
+    mod_path=/mnt/live/memory/images
+ 
+    mount_br=$mod_br
+ 
+    rm -rf $mount_br $root_br
+    mkdir -p $mount_br $root_br
+ 
+    for a in `ls -d $mod_path/??-base*`; do
+	mount_br="$mount_br:$a=rr+nowh"
+    done
+ 
+    mount -t aufs -o br:$mount_br wiz_fly $root_br
+    mkdir -p $root_br/tmp
+#    /usr/sbin/urpmi --root=$root_br $@
+    /usr/sbin/urpmi $URPMI_PARAM --noclean --root=$root_br $@
+    umount wiz_fly
+    rm -rf $mod_br/var/lib/rpm
+    rm -rf $mod_br/tmp
+    if [ $? != 0 ]; then echo "error building modules "; exit 1; fi
+    dir2lzm $mod_br $mod_name
+
+    rm -rf $mod_br
+}
+#---------------
+
+
+MOD_REP=repository/optional
+MOD_NAMES_DIR=mod_names
+#URPMI_PARAM="--no-verify-rpm --excludemedia MIB"
+URPMI_PARAM="--no-verify-rpm "
+
+mkdir -p  $MOD_REP
+mask=$1
+for mod in `ls -1 $MOD_NAMES_DIR/??-add-*$mask` ;do 
+    echo "Генерация метамодуля $(basename $mod)"
+    if [ -d $MOD_REP/$(basename $mod) ] ;then 
+	echo "...директория уже создана"
+#	continue
+    else
+	mkdir -p $MOD_REP/$(basename $mod)
+    fi
+
+#--------------
+    pak_list=""
+     for pak in `cat $mod` ;do 
+        echo -ne \\r "Добавляется пакет $pak         "\\r 
+        pak_list="$pak_list $pak"
+#        sleep 3
+#        urpmi --root=$rootfs --noclean --download-all $MOD_LOADED $1 $pak
+#        urpmi --root=$rootfs --noclean $1 $pak
+     done
+     make_mod_pack $MOD_REP/$(basename $mod)/$(basename $mod)-$(date +%Y%m%d).xzm $pak_list
+#--------------
+    echo -ne \\n "---> OK."\\n
+done
+#     umount $rootfs
