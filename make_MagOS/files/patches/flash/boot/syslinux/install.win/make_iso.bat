1d3173153df9c29a523be3a34189be7492c34890
magos-linux <m3for@mail.ru>
2012-12-15 21:33:14 +0400

    grub4dos update

--- make_MagOS/files/patches/flash/boot/syslinux/install.win/make_iso.bat
+++ make_MagOS/files/patches/flash/boot/syslinux/install.win/make_iso.bat
@@ -9,7 +9,7 @@ cd ..\..\..\
 set CDLABEL=MagOS
 set ISOPATH=..
 if exist "..\MagOS" set ISOPATH=c:
-boot\tools\WIN\mkisofs.exe @boot\syslinux\iso_conf -o "%ISOPATH%\%CDLABEL%.iso" -A "%CDLABEL%" -V "%CDLABEL%" -m '*_save*' MagOS=MagOS boot=boot isolinux.cfg=boot/syslinux/isolinux.cfg
+boot\tools\WIN\mkisofs.exe @boot\syslinux\iso_conf -o "%ISOPATH%\%CDLABEL%.iso" -A "%CDLABEL%" -V "%CDLABEL%" -m '*_save*' MagOS=MagOS boot=boot isolinux.cfg=boot/syslinux/syslinux.cfg
 echo.
 echo New ISO should be created now. See %ISOPATH%\%CDLABEL%.iso
 
--- make_MagOS/files/patches/rootfs/MagOS_2012lts-filesystem.sh
+++ make_MagOS/files/patches/rootfs/MagOS_2012lts-filesystem.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 mkdir -m 755 -p run/udev/rules.d run/lock
+rm -f lib/systemd/system/basic.target.wants/mandriva-boot-links.service \
+      lib/systemd/system/local-fs.target.wants/fsck-root.service \
+      lib/systemd/system/local-fs.target.wants/remount-rootfs.service \
+      etc/systemd/system/multi-user.target.wants/shorewall.service
 exit 0
