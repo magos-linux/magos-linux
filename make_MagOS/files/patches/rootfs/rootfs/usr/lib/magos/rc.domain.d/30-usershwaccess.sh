5d53ec900a720a1e63ead4d544defd9cf53989e6
magos-linux <m3for@mail.ru>
2012-12-22 22:49:02 +0400

    gfxmenu added for grub4dos

--- make_MagOS/files/patches/rootfs/rootfs/usr/lib/magos/rc.domain.d/30-usershwaccess.sh
+++ make_MagOS/files/patches/rootfs/rootfs/usr/lib/magos/rc.domain.d/30-usershwaccess.sh
@@ -5,5 +5,7 @@ USERGROUPS=audio,video,usb,vboxusers
 [ -f /etc/sysconfig/MagOS ] && . /etc/sysconfig/MagOS
 [ -z "$DOMAINUSERSHWACCESS" ] && exit 0
 for a in $(echo "$DOMAINUSERSHWACCESS" | tr ",;" " ") ;do
-    usermod -a -G $USERGROUPS $a
+   for b in $(echo "$USERGROUPS" | tr ",;" " ") ;do
+       usermod -a -G $b $a
+   done
 done
