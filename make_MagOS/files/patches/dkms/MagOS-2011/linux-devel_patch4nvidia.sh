693356a7bdb7e13b101716ca8ae2b5bcf62e952f
magos-linux <m3for@mail.ru>
2013-04-21 20:23:46 +0400

    cleanup deleted files

--- make_MagOS/files/patches/dkms/MagOS-2011/linux-devel_patch4nvidia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CPATH=$PWD
-cd usr/src/kernel-magos-devel-* 2>/dev/null || cd usr/src/linux-*
-mkdir -p arch/x86/include/asm
-cp -p arch/x86/include/generated/asm/* arch/x86/include/asm
-touch arch/x86/include/asm/system.h
-cd $CPATH
