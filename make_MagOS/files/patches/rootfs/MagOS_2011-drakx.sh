a7b0a7045ca5884158fdfc65d04dc5ed9f6c84c1
magos-linux <m3for@mail.ru>
2013-04-21 20:15:17 +0400

    cleanup deleted files

--- make_MagOS/files/patches/rootfs/MagOS_2011-drakx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PFP=usr/lib/libDrakX/ugtk2.pm
-sed -i s/'->new_with_status_icon($info->{title}, $info->{message}, undef, $self->{statusicon})'/'->new($info->{title}, $info->{message}, $self->{statusicon})'/ $PFP
-PFP=usr/lib/libDrakX/network/tools.pm
-sed -i 's|foreach (cat_("/proc/net/route"))|my @routes = cat_("/proc/net/route");\n    require bootloader;\n    @routes = reverse(@routes) if bootloader::cmp_kernel_versions(c::kernel_version(), "2.6.39") >= 0;\n    foreach (@routes)|' $PFP
-
-exit 0
--- make_MagOS/files/patches/rootfs/MagOS_2011-lirc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's|lirc/|lirc|' etc/udev/rules.d/lirc.rules
