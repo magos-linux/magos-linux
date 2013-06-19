e2dcc0ef139b71aa03e3473baf271018942a84e4
magos-linux <m3for@mail.ru>
2013-06-19 20:19:46 +0400

    2012lts: dbus, desktop script fixed

---
+++ make_MagOS/files/patches/rootfs/MagOS-dbus.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#BUGFIX Sometimes dbus package are installed with incorrect group owner
+#BUGFIX It does not allow to launch any gtk apps under Gnome
+chroot ./ /bin/chgrp messagebus /lib/dbus-1/dbus-daemon-launch-helper
+exit 0
