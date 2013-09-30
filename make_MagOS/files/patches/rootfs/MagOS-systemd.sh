511777a834e50b1304e4f3b9a5106873bd7705a2
magos-linux <m3for@mail.ru>
2013-09-30 06:15:50 +0400

    release 20130929

--- make_MagOS/files/patches/rootfs/MagOS-systemd.sh
+++ make_MagOS/files/patches/rootfs/MagOS-systemd.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 SERVICESSTOP=abrtd,hostapd,irqbalance,smartd
 for a in $SERVICESSTOP ;do 
-   rm -f etc/systemd/system/multi-user.target.wants/$a.service
-   [ -f "lib/systemd/system/$a.service" ] &&  sed -i s/WantedBy=.*// lib/systemd/system/$a.service
+    ln -s '/dev/null' "etc/systemd/system/$a.service"
 done
-
 exit 0
-
