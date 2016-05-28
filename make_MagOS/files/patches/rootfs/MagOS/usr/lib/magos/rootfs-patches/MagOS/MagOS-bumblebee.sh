836e3d6338723a5efb2a6054d1f2ab6472fd0906
magos-linux <m3for@mail.ru>
2016-05-28 14:01:35 +0300

    release 20160527

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-bumblebee.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-bumblebee.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PFP=/etc/bumblebee/bumblebee.conf
 sed -i s/^Driver=.*$/Driver=nvidia/ $PFP
-sed -i s/^KernelDriver=$/KernelDriver=nvidia-current/ $PFP
+sed -i s/^KernelDriver=$/KernelDriver=nvidia/ $PFP
 exit 0
