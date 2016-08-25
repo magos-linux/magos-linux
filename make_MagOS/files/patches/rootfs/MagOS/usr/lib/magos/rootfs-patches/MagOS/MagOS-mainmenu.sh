2556dcb935c94249b5d65a5a443aa3f36b52eaac
magos-linux <m3for@mail.ru>
2016-08-25 12:55:13 +0300

    release 20160824

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-mainmenu.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-mainmenu.sh
@@ -75,6 +75,7 @@ update_app lxshortcut.desktop "GTK;X-LXDE-Settings;"
 update_app drakxservices.desktop "Settings;System;X-MandrivaLinux-CrossDesktop;"
 update_app XFdrake.desktop "GTK;Settings;HardwareSettings;"
 update_app vidalia.desktop "Qt;Utility;Security;"
+update_app devedeng.desktop "AudioVideo;DiscBurning;"
 
 update_app kde4/akonaditray.desktop "Qt;KDE;X-KDE-Utilities-PIM;"
 update_app kde4/dolphin.desktop "Qt;KDE;System;Utility;Core;FileManager;"
