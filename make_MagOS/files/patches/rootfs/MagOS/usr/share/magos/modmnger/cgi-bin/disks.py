370564621024683d5cb637a875be25dd0e965175
betcher <betkher.al@mail.ru>
2017-09-13 21:54:52 +0300

    fixed disk.py

--- make_MagOS/files/patches/rootfs/MagOS/usr/share/magos/modmnger/cgi-bin/disks.py
+++ make_MagOS/files/patches/rootfs/MagOS/usr/share/magos/modmnger/cgi-bin/disks.py
@@ -12,54 +12,56 @@ def getPhysical():
 
 def getLogical():
 	arr = {}
-	command = ('/sbin/blkid -s TYPE  -s LABEL |grep  "/dev/sd"')
+	arrmnt = getmntArr()
+	command = ('beesu /sbin/blkid -s TYPE  -s LABEL |grep  "/dev/sd"')
 	for key_val  in os.popen( command ).read().split('\n'):
-		if len(key_val.split()) == 3:
-			command = ( 'df -h  ' +  key_val.split()[0].strip(':'))
-			df = os.popen( command ).read().split('\n')[1].split()[1:6]
-			arr[key_val.split()[0].strip(':')] = [key_val.split()[1].replace('LABEL=', '').strip('"'), ]
-			arr[key_val.split()[0].strip(':')].append (key_val.split()[2].replace('TYPE=', '').strip('"'))
-			for a in df:
-				arr[key_val.split()[0].strip(':')].append(a)
-		if len(key_val.split()) == 2: 
-			command = ( 'df -h  ' +  key_val.split()[0].strip(':'))
-			df = os.popen( command ).read().split('\n')[1].split()[1:6]
-			arr[key_val.split()[0].strip(':')] = ['nolabel', ]
-			arr[key_val.split()[0].strip(':')].append (key_val.split()[1].replace('TYPE=', '').strip('"'))	
-			for a in df:
-				arr[key_val.split()[0].strip(':')].append(a)
-			
-	
-	
+		if len(key_val.split()) > 1:
+			if len(key_val.split()) == 3:
+				arr[key_val.split()[0].strip(':')] = [key_val.split()[1].replace('LABEL=', '').strip('"'), ]
+				arr[key_val.split()[0].strip(':')].append (key_val.split()[2].replace('TYPE=', '').strip('"'))
+			if len(key_val.split()) == 2: 
+				arr[key_val.split()[0].strip(':')] = ['nolabel', ]
+				arr[key_val.split()[0].strip(':')].append (key_val.split()[1].replace('TYPE=', '').strip('"'))	
+			if key_val.split()[0].strip(':') in arrmnt:
+				for a in arrmnt[key_val.split()[0].strip(':')] :
+					arr[key_val.split()[0].strip(':')].append(a)
 	for  key, val in arr.items():
-		if val[6] != '/dev':
+		if len(val) == 7 :
 			dirls = os.listdir(val[6])
 			if len(dirls) > 0:
 				arr[key] = arr.get(key), dirls
 			else:
 				arr[key] = arr.get(key), ['disk empty',]
 		else:
-			val[2] = '---'
-			val[3] = '---'
-			val[4] = '---'
-			val[5] = '---'
-			val[6] = '---'
+			add = [ '---', '---','---', '---', '---' ]
+			arr[key] = val + add 
 			if  val[0] != 'swap':
 				arr[key] = arr.get(key), ['is not mounted',]
 			else:
 				arr[key] = arr.get(key), ['swap partition',]
 	return arr
 
-	
+
+def getmntArr ():
+	arrmnt = {}
+	command2 = ( 'findmnt -lnC -o SOURCE,SIZE,USED,AVAIL,USE%,TARGET |grep sd' )
+	for line  in os.popen( command2 ).read().split('\n'):
+		if len(line.split()) > 5:
+			arrmnt[line.split()[0] ] =  ( line.split()[1], line.split()[2], line.split()[3], line.split()[4], line.split()[5] ) 
+	return arrmnt
 
 if __name__ == '__main__':
 
 	physical = getPhysical()
 	logical =  getLogical()
+	mnt = getmntArr()
 
 	print logical
 	print '-----------------------------------***'
 	print physical 
+	print '-----------------------------------***'
+	print mnt
+	
 	
 	#for key, val in physical.items():
 	#	print 'disk:' + key + '  ' + val[0] + '  ' +val[1]
