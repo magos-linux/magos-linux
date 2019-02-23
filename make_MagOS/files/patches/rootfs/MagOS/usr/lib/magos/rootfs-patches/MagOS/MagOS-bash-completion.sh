61e9e9e100ec87c7499c7c7062ee64cc9871452f
magos-linux <m3for@mail.ru>
2019-02-23 10:18:24 +0300

    release 20190222

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-bash-completion.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-bash-completion.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PFP=/etc/bash_completion.d/urpmi
 if ! grep -q "urpm2xzm" $PFP ;then
-   sed -i s%"urpmi gurpmi rurpmi"$%"urpmi gurpmi rurpmi urpm2pfs urpm2xzm"% $PFP
-   sed -i s%"for everything else as rurpmi"$%"for everything else as rurpmi"\\n'[[ ${COMP_WORDS[0]} == *urpm2pfs ]] \&\& options=\"$options -m --mask -o -n --name -e --erase -r --rpmdb -u --urpmdb -urpmi --urpmi'\"% $PFP
+   sed -i s%"urpmi gurpmi rurpmi"$%"urpmi gurpmi rurpmi urpm2xzm"% $PFP
+   sed -i s%"for everything else as rurpmi"$%"for everything else as rurpmi"\\n'[[ ${COMP_WORDS[0]} == *urpm2pfs ]] \&\& options=\"$options --help --mask --rpmdb --urpmidb --erase --rebuildable --fast --steps --bulddir --load --info -urpmi --urpmi'\"% $PFP
 fi
 
 exit 0
