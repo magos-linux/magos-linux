381878b2397359d6cff8ae7f4694b4e3bb748a81
magos-linux <m3for@mail.ru>
2019-05-11 08:39:03 +0300

    release 20190511

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-mainmenu.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-mainmenu.sh
@@ -89,6 +89,7 @@ update_app obconf-qt.desktop "Qt;DesktopSettings;X-LXQT-Settings;"
 update_app pcmanfm-qt-desktop-pref.desktop "Qt;DesktopSettings;X-LXQT-Settings;"
 update_app compton-conf.desktop "Qt;DesktopSettings;X-LXQT-Settings;"
 update_app lftp.desktop "Network;FileTransfer;"
+update_app io.github.qtox.qTox.desktop "Network;Chat;Telephony;"
 
 update_app kde4/akonaditray.desktop "Qt;KDE;X-KDE-Utilities-PIM;"
 update_app kde4/dolphin.desktop "Qt;KDE;System;Utility;Core;FileManager;"
