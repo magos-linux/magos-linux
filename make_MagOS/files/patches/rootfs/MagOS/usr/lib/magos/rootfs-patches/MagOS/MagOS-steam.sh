5e7a22cd1ed9c7e43366825bce6da5cb57db4c45
magos-linux <m3for@mail.ru>
2020-03-27 21:49:35 +0300

    release 20200326

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-steam.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-steam.sh
@@ -2,7 +2,12 @@
 # drive bar was disabled because it shows all loop devices(
 [ -x /usr/bin/steam ] || exit 0
 FILE=/usr/share/applications/steam.desktop
+FILEBP=/usr/share/applications/steam-bp.desktop
 [ -f $FILE ] && sed -i s%"Exec=/usr/bin/steam"%"Exec=/usr/lib/magos/scripts/startsteam"% $FILE
+[ -f $FILEBP ] || cp -p $FILE $FILEBP
+sed -i s/=Steam$/"=Steam Big Picture"/ $FILEBP
+sed -i s%Exec=.*%"Exec=/usr/lib/magos/scripts/startsteam-bp"% $FILEBP
+
 FILE=/etc/X11/wmsession.d/43STEAM
 [ -f $FILE ] && exit 0
 cat >>$FILE <<EOF
