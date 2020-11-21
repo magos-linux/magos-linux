5ffdd0c3479da584c05be8dce70087fb357b33ee
magos-linux <m3for@mail.ru>
2020-11-21 11:26:27 +0300

    release 20201120

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-bumblebee.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-bumblebee.sh
@@ -2,4 +2,6 @@
 PFP=/etc/bumblebee/bumblebee.conf
 sed -i s/^Driver=.*$/Driver=nvidia/ $PFP
 sed -i s/^KernelDriver=nvidi.*$/KernelDriver=nvidia-current/ $PFP
+sed -i s/'#[[:space:]]*BusID "PCI:'/'    BusID "PCI:'/ /etc/bumblebee/xorg.conf.nouveau
+sed -i s/'#[[:space:]]*BusID "PCI:'/'    BusID "PCI:'/ /etc/bumblebee/xorg.conf.nvidia
 exit 0
