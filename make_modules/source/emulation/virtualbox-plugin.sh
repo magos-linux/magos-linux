a001c3082e64a4df10abbd90e7b7d10ee99f6e15
magos-linux <m3for@mail.ru>
2020-12-25 21:11:45 +0300

    release 20201224

---
+++ make_modules/source/emulation/virtualbox-plugin.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+NUM=81
+NAME=virtualbox-plugin
+RELEASE=$(date +%Y%m%d)
+DISTR=2016
+AUTHOR=dont_redistribute
+VERSION=$(rpm -qq virtualbox | awk -F- '{print $2}')
+
+SOURCE0=https://download.virtualbox.org/virtualbox/${VERSION}/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
+DOWNLOADER=wget
+
+. ../functions || exit 1
+
+clean
+
+prepare
+
+download $SOURCE0
+
+mv -f ${SOURCE0##*/} ${SOURCE0##*/}.tar.gz
+mkdir -p "rootfs/usr/lib64/virtualbox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack"
+unpack ${SOURCE0##*/}.tar.gz "rootfs/usr/lib64/virtualbox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack"
+
+defaultrights rootfs
+
+echo "$DIRNAME"
+
+pack rootfs "$DIRNAME"
+
+clean
