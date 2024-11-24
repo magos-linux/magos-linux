1ce3bb1b8f3138fb7163ed660e21e446d122f6ab
magos-linux <maglinux@yandex.ru>
2024-11-24 08:09:54 +0300

    release 20241122

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-doublecmd.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS/MagOS-doublecmd.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+VER="-qt5"
+[ -x /usr/bin/doublecmd$VER ] || VER="-qt6"
+[ -x /usr/bin/doublecmd$VER ] || VER="-qt"
+[ -x /usr/bin/doublecmd$VER ] || VER=""
+[ -x /usr/bin/doublecmd$VER ] || exit 0
 
-[ -x /usr/bin/doublecmd-qt ] || exit 0
+[ -f /usr/share/applications/doublecmd.desktop ] || mv -f /usr/share/applications/doublecmd$VER.desktop /usr/share/applications/doublecmd.desktop 2>/dev/null
 
-[ -f /usr/share/applications/doublecmd.desktop ] || mv -f /usr/share/applications/doublecmd-qt.desktop /usr/share/applications/doublecmd.desktop
-
-PFP=/usr/lib/doublecmd
-[ -d "$PFP" ] || PFP=/usr/lib64/doublecmd
+PFP=/usr/lib64/doublecmd
 [ -d "$PFP" ] || exit 0
 PFP=$PFP/doublecmd.xml
 [ -f "$PFP" ] || echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<doublecmd DCVersion=\"0.9.10 beta\" ConfigVersion=\"10\">\n</doublecmd>" > $PFP
