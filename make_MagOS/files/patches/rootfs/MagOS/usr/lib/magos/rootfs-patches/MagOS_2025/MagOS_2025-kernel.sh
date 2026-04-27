91da2db0d9f956429b27f218c6a5a8107bd8ece7
magos-linux <maglinux@yandex.ru>
2026-04-27 22:23:32 +0300

    release 20260426

--- make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS_2025/MagOS_2025-kernel.sh
+++ make_MagOS/files/patches/rootfs/MagOS/usr/lib/magos/rootfs-patches/MagOS_2025/MagOS_2025-kernel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # remove conflict data
 NVIDIAMODS="nvidia580 nvidia470"
+KERN=$(ls -d /usr/lib/modules/*desktop* | tail -1 | sed 's|.*/||')
 mv /usr/lib/modules/*desktop*/kernel/drivers/video/nvidia* /
 for a in $NVIDIAMODS ;do
   mv /$a /usr/lib/modules/*desktop*/kernel/drivers/video
 done
-depmod -a
+depmod -a $KERN
 mv /nvidia* /usr/lib/modules/*desktop*/kernel/drivers/video
